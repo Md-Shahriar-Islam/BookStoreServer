602526693e64c62ab299c5f60cad32b817b2e3df
Md Shahriar Islam <mdshahriarislam23@gmail.com>
2022-05-08 00:34:24 +0600

    data fetched for different user completed

--- index.js
+++ index.js
@@ -10,19 +10,62 @@ app.get('/', (req, res) => {
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a24qz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client1 = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         await client.connect()
+        await client1.connect()
         const bookCollection = client.db("library").collection('books')
+        const myBookCollection = client1.db("library").collection('mybook')
         app.get("/inventory", async (req, res) => {
             const query = {}
             const cursor = bookCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
         })
+        app.get("/inventory/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const cursor = await bookCollection.findOne(query)
+            res.send(cursor)
+        })
+        app.put('/inventory/:id', async (req, res) => {
+            const id = req.params.id
+            const variable = req.body
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            console.log('Hello')
+
+            const updateDoc = {
+                $set: {
+                    quantity: variable.quantity
+                }
+            };
+            const result = await bookCollection.updateOne(filter, updateDoc, options);
+
+        })
+        app.post("/mybook", async (req, res) => {
+            const book = req.body
+            console.log((req.body))
+            console.log(book)
+            const result = await myBookCollection.insertOne(book)
+
+
+
+
+        })
+        app.get("/mybook", async (req, res) => {
+            const data = req.query
+            console.log(data)
+            const cursor = myBookCollection.find(data);
+            const result = await cursor.toArray()
+
+            res.send(result)
+        })
+
 
 
     }
